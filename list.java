31bb5241e14bda6594843c353da43155562198d1
Gawsan <gawsan98@gmail.com>
2022-01-02 23:29:33 +0530

    instantiate the List interface

---
+++ list.java
@@ -0,0 +1,18 @@
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Stack;
+import java.util.Vector;
+
+class list{
+
+   public static void main(String [] args){
+List <String> list1= new ArrayList();  
+List <String> list2 = new LinkedList();  
+List <String> list3 = new Vector();  
+List <String> list4 = new Stack();  
+
+list1.add("Gawsan");
+list2.add("Raveenthiran");
+   }
+}
